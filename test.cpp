bf5cd7a704c7a0e8e1300e29922713c0ead21ca9
shubham-pie <163717438+shubham-pie@users.noreply.github.com>
2024-03-18 16:13:51 +0530

    0.1

---
+++ test.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	const double pi = 3.14;
+	// Square Root
+	/*
+	int a;
+	double ans;
+	cout << "I want square root of: \n";
+	cin >> a;
+	ans = sqrt(a);
+	cout << "square root of " << a << " is " << ans;
+	*/
+
+	// Area of Cirlce
+	/*
+	double radius, area;
+	
+	cout << "Enter value of the radius ";
+	cin >> radius;
+	area = radius * pi * radius;
+	cout << "Area of circle with radius " << radius <<" is "<< area;
+	*/
+
+	// cIRCUMFERNECE of circle
+	/*
+	int radii;
+	cout  << "Enter the radius ";
+	cin >> radii;
+	double circum = 2* radii * pi;
+	cout << "Circumference is " << circum;
+	*/
+
+	//program to find maximum between maximum between two numbers
+	/*int a,b;
+	cout << "Enter 2 numbers: ";
+	cin >> a >> b;
+	if (a>b){
+		cout << a <<" is greater";
+	}
+	else if (a<b){
+		cout << b << " is greater";
+	}
+
+	else {
+		cout << "both are equal";
+	}
+	*/
+
+	// To find maximum among three numbers
+	/*
+	int a, b, c;
+	cout << "Enter 3 numbers: ";
+	cin >>a>>b >>c;
+	if ((a>b) && (a>c)){
+		cout << a <<" is greater";
+	}
+
+	else if ((b>a) && (b>c)){
+		cout << b <<" is greater";
+	}
+
+	else if ((c>a) && (c>b)){
+		cout << c<<" is greater";
+	}
+
+	else {
+		cout<< "All are equal";
+	}
+	*/
+
+
+	return 0;
+}
